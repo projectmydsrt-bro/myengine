70b9e6c985d22b76fb7bb08d83b3062ee850d3c8
projectmydsrt-bro <projectmydsrt@gmail.com>
2025-09-18 16:39:16 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+export { Engine } from "./src/engine.js";
+export { Scene } from "./src/scene.js";
+export { PerspectiveCamera } from "./src/camera.js";
+export { BoxGeometry } from "./src/geometry.js";
+export { Mesh } from "./src/mesh.js";
+export { Renderer } from "./src/renderer.js";
